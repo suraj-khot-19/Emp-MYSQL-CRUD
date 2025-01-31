0ac99c9f57fed66b0502202e3ef6b2cee5044c8a
suraj-khot-19 <surajkhot19082001@gmail.com>
2025-01-31 22:10:43 +0530

    initial commit

---
+++ src/main/java/com/mysql/curd/employee/EmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.mysql.curd.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+}
